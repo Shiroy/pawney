af43246024db768273c7c63b500d4e975448efc7
Antoine Wacheux <awacheux.utc@gmail.com>
2023-11-29 13:55:55 +0100

    Validate the session on every request

--- packages/pawney-app/src/app.d.ts
+++ packages/pawney-app/src/app.d.ts
@@ -1,9 +1,14 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
+
+import type { UserInfo } from "$lib/shared/user";
+
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user?: UserInfo;
+		}
 		// interface PageData {}
 		// interface Platform {}
 	}
---
+++ packages/pawney-app/src/hooks.server.ts
@@ -0,0 +1,33 @@
+import type { Handle } from "@sveltejs/kit"
+import { sequence } from "@sveltejs/kit/hooks";
+import { stytchClient } from "$lib/server/stytch";
+
+const validateSession: Handle = async ({ event, resolve }) => {
+    const session_token = event.cookies.get('session_token');
+
+    if(session_token) {
+        try {
+            const { user, session_token: new_session_token } = await stytchClient.sessions.authenticate({ 
+                session_token,
+                session_duration_minutes: 60 * 24 * 30 // 30 days
+             });
+
+            event.locals.user = { user_id: user.user_id, email: user.emails[0].email };
+
+            // Update the cookie with a new session token.
+            event.cookies.set('session_token', new_session_token, {
+                path: '/',
+                secure: true,
+                httpOnly: true,
+                sameSite: 'none',
+                maxAge: 60 * 60 * 24 * 30,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    return resolve(event);
+}
+
+export const handle = sequence(validateSession);
